a1da163be4889e1f8ae5e8bc0d4ebe992346efa5
Grant Y Hughes <granthughes93@gmail.com>
2016-08-18 13:58:59 -0700

    Added Python script

---
+++ AutoVRBO-Python/AutoVRBO_Python.py
@@ -0,0 +1,34 @@
+import time
+import sys
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+# Assign arguments.
+if len(sys.argv) != 3:
+    print 'Error. Usage: AutoVRBO_Python.py username password'
+    sys.exit()
+un = str(sys.argv[1])
+pw = str(sys.argv[2])
+
+# Load webpage.
+browser = webdriver.Firefox()
+browser.get('http://www.vrbo.com/haod')
+browser.implicitly_wait(3)
+
+# Login.
+username = browser.find_element_by_name("username")
+username.send_keys(un)
+password = browser.find_element_by_name("password")
+password.send_keys(pw)
+submit = browser.find_element_by_id("form-submit")
+submit.click()
+
+# Python 2.7
+# pip install -U selenium
+# Refresh calendar.
+update = browser.find_element_by_id("republishCalendarButton")
+update.click()
+
+# Close app.
+browser.close()
+browser.quit()
